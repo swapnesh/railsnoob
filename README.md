6336a46e41dfd02dea392856c37273f8285119a9
johncausey <martin.causey@gmail.com>
2013-05-11 08:22:56 -0700

    rebuild action

---
+++ README.md
@@ -0,0 +1,11 @@
+railsnoob
+=========
+
+This is an open source project for railsnoob. currently active at irc://freenode/railsnoob
+
+Active members- 
+
+Proshot
+Leighton
+Swapnesh
+Banas
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsnooblocal::Application.config.secret_token = 'b7f2f787b285c22e1a247278782b46d9009da61d58315deb70d8cfafd4250ee01e9111bfe66c42c1eaa29eda89db7bfc652eacf020804cb7837a9632ef223330'
