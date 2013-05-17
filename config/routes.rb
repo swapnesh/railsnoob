36c1d2711e85ffcfc1d40828715a13ce04b66e35
swapnesh <swapnesh20032003@gmail.com>
2013-05-17 22:08:35 +0530

    Created partials menu and footer, located under shared folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Railsnooblocal::Application.routes.draw do
+  get "contact/index"
+
+  get "register/index"
+
+  get "user/index"
+
+  get "project/index"
+
+  get "about/index"
+
   get "home/index"
 
   # The priority is based upon order of creation:
@@ -57,5 +67,5 @@ Railsnooblocal::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+   match ':controller(/:action(/:id))(.:format)'
 end
